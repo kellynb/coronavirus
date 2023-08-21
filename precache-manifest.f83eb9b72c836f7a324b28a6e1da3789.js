6473e7094b69560f5b2ae05189b61cd1ccde2ce8
kellynb <bkellyn013@gmail.com>
2023-08-21 15:52:49 -0500

    Updates

---
+++ precache-manifest.f83eb9b72c836f7a324b28a6e1da3789.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cffc0fbda237b75d934b7f56259e19",
+    "url": "/coronavirus/index.html"
+  },
+  {
+    "revision": "b5cb95dd1ea6f42e85fc",
+    "url": "/coronavirus/static/js/2.e1725806.chunk.js"
+  },
+  {
+    "revision": "a712f5023b98f1ee6f879156125341ec",
+    "url": "/coronavirus/static/js/2.e1725806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502c688f293a7e6fef74",
+    "url": "/coronavirus/static/js/main.7d4be7b0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/coronavirus/static/js/main.7d4be7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f907e94a774fe8c6966c",
+    "url": "/coronavirus/static/js/runtime-main.82109013.js"
+  },
+  {
+    "revision": "88393cd8e35575010d3c121768597799",
+    "url": "/coronavirus/static/media/death_face.88393cd8.png"
+  },
+  {
+    "revision": "2f8f9615748f6bc87dfdece82d60fe01",
+    "url": "/coronavirus/static/media/hear_face.2f8f9615.png"
+  },
+  {
+    "revision": "26eaaf0f7219b2ae73cfda314b3eab9a",
+    "url": "/coronavirus/static/media/neutral.26eaaf0f.png"
+  },
+  {
+    "revision": "b46ea8188b7482d165918d54de817ccc",
+    "url": "/coronavirus/static/media/oozy_face.b46ea818.png"
+  },
+  {
+    "revision": "2b2597e5b5f77ee2a565c2e39f9a9106",
+    "url": "/coronavirus/static/media/smile.2b2597e5.png"
+  }
+]);
